21438c75a5ce883c97f4245f24dd59ca77db61ee
Raffi <raffi38321@gmail.com>
2026-02-14 15:10:36 +0700

    feat:filter by category

---
+++ src/components/TagCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TagCard = ({ tag, handleFilterTag }) => {
+  const onFilterTag = () => {
+    handleFilterTag({ text: tag });
+  };
+
+  return (
+    <p
+      className="p-5 bg-white border-4 border-black shadow-[6px_6px_0_0_#000] font-bold"
+      onClick={onFilterTag}
+    >
+      #{tag}
+    </p>
+  );
+};
+
+export default TagCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {asyncPopulateUsersAndThreads} from '../states/shared/action';
-import ThreadCard from '../components/ThreadCard';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncPopulateUsersAndThreads } from "../states/shared/action";
+import ThreadCard from "../components/ThreadCard";
+import TagCard from "../components/TagCard";
 
 const HomePage = () => {
   const {
@@ -10,19 +11,45 @@ const HomePage = () => {
     users = [],
     authUser,
   } = useSelector((states) => states);
+
   const dispatch = useDispatch();
+  const [filteredThreads, setFilteredThreads] = useState([]);
+
   useEffect(() => {
     dispatch(asyncPopulateUsersAndThreads());
   }, [dispatch]);
 
-  const addUserToThreads = threads.map((thread) => ({
+  useEffect(() => {
+    setFilteredThreads(threads);
+  }, [threads]);
+
+  const addUserToThreads = filteredThreads.map((thread) => ({
     ...thread,
     user: users.find((user) => user.id === thread.ownerId),
     authUser,
   }));
 
+  const tags = [...new Set(threads.map((t) => t.category))];
+
+  const handleFilterTag = ({ text }) => {
+    if (filteredThreads[0].category === text) {
+      setFilteredThreads(threads);
+    } else {
+      const result = threads.filter((thread) => thread.category === text);
+      setFilteredThreads(result);
+    }
+  };
   return (
     <div>
+      <div className="flex flex-row gap-5">
+        {tags.map((textTag, index) => (
+          <TagCard
+            key={index}
+            tag={textTag}
+            handleFilterTag={handleFilterTag}
+          />
+        ))}
+      </div>
       {addUserToThreads.map((thread) => (
         <ThreadCard key={thread.id} thread={thread} />
       ))}
